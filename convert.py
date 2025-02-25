00d90be0dd230d8bbc7eb0da82f9484d8a9a12d7
fikasp <fikasp@gmail.com>
2025-02-25 20:33:28 +0100

    Update 2020

--- convert.py
+++ convert.py
@@ -3,7 +3,7 @@ from PIL import Image
 # pip install Pillow
 
 # Input folder path
-input_folder = 'b:/Prywatne/Lifebook/Timeline/2023/'
+input_folder = 'b:/Prywatne/Lifebook/Timeline/2020/'
 
 # Subfolder for webp
 webp_folder = os.path.join(input_folder, 'webp')
