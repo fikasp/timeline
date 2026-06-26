6718b102d14d80d7b66d29af1b4eaf2a9f942b3b
fikasp <fikasp@gmail.com>
2026-06-26 05:32:45 +0200

    Add 2026-06-20

--- index.js
+++ index.js
@@ -1820,5 +1820,6 @@ const data = [
 {date: '2026-05-31.2', catg: ['Kościoły'], name: 'Kościół Sercanek'},
 {date: '2026-05-31.3', catg: ['Gastronomia'], name: 'Restauracja Mythos'},
 {date: '2026-06-13.0', catg: ['Koncerty'], name: 'Koncert w Mariackim'},
+{date: '2026-06-20.0', catg: ['Rowery'], name: 'Rower Bronowice'},
 {date: '2026-06-21.0', catg: ['Koncerty'], name: 'Koncert u Szczepana'},
 ]
