d3496670860a95afa853722cb22b5dfd0f03619e
fikasp <fikasp@gmail.com>
2026-07-28 00:33:16 +0200

    Add 2026-07-26

--- index.js
+++ index.js
@@ -1826,4 +1826,5 @@ const data = [
 {date: '2026-06-27.0', catg: ['Rodzina', 'Uroczystości'], name: 'Ślub Beatki Sypek'},
 {date: '2026-06-28.0', catg: ['Rowery'], name: 'Rower Tonie'},
 {date: '2026-06-30.0', catg: ['Koncerty'], name: 'Koncert u Karmelitów'},
+{date: '2026-07-26.0', catg: ['Rodzina'], name: 'Imieniny taty'},
 ]
